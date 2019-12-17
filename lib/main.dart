bfe8b0dd952802e1c7fd99c06abd25b8d3aadb57
Chakradhar <chakriu.u@gmail.com>
2019-12-17 10:52:45 +0530

    FIrebase files

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      child: Text('dgdfg'),
+    );
+  }
+  
+}
